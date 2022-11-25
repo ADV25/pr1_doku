6476d244936362ec5e0fed26c7df4382a8aaf20e
bdwebnet <bdwebnet@users.noreply.github.com>
2022-11-25 22:16:45 +0000

    Deployed e467abc with MkDocs version: 1.3.1

---
+++ javascripts/extra.js
@@ -0,0 +1,9 @@
+function externalLinks() {
+    let c = document.getElementsByTagName("a"), a = 0;
+    for(; a < c.length; a++) {
+        var b = c[a];
+        b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+    }
+}
+
+externalLinks();
