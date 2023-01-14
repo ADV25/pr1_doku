002230a1acd4ce586d707744571a70c2b470afb4
bd <51322242+bdwebnet@users.noreply.github.com>
2023-01-14 15:25:29 +0100

    better navigation

--- docs/assets/javascripts/extra.js
+++ docs/assets/javascripts/extra.js
@@ -1,6 +1,6 @@
 function externalLinks() {
     let c = document.getElementsByTagName("a"), a = 0;
-    for(; a < c.length; a++) {
+    for (; a < c.length; a++) {
         var b = c[a];
         b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
     }
